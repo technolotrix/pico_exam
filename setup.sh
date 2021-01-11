19e766c28dbb764c8d2a6dcc2d80397bfdca1a0d
technolotrix <technolotrix@gmail.com>
2021-01-11 15:09:24 -0500

    config and venv

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-source .env
+python -m venv qa_dev
+source qa_dev/bin/activate
+pip install -r requirements.txt
 
 # nosetests -vs tests/test_onboarding_wizard.py &
 bash tests/test_performance.sh
